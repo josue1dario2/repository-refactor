c8762902734280871e4359a9a1bbce7529f61276
Josue Solis <josue1dario2solis3@gmail.com>
2022-05-14 11:45:30 -0300

    initial commit

---
+++ src/test/java/com/proyect/tinder/TinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.tinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
