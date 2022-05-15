0990477285c9d97b0668e5d118f2c1e469af3ea2
Josue Solis <josue1dario2solis3@gmail.com>
2022-05-15 13:38:44 -0300

    create class service user

--- src/main/java/com/proyect/tinder/services/UserService.java
+++ src/main/java/com/proyect/tinder/services/UserService.java
@@ -1,2 +1,15 @@
-package com.proyect.tinder.services;public interface UserService {
+package com.proyect.tinder.services;
+
+import com.proyect.tinder.models.User;
+
+public interface UserService {
+
+    User saveUser(User user);
+
+    User updateUser(User user ,Long id);
+
+    Void enableUser(Long id);
+
+    Void disableUser(Long id);
+
 }
--- src/main/java/com/proyect/tinder/services/impl/UserServiceImpl.java
+++ src/main/java/com/proyect/tinder/services/impl/UserServiceImpl.java
@@ -1,2 +1,40 @@
-package com.proyect.tinder.services.impl;public class UserServiceImpl {
+package com.proyect.tinder.services.impl;
+
+import com.proyect.tinder.models.User;
+import com.proyect.tinder.repositories.UserRepository;
+import com.proyect.tinder.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(User user, Long id) {
+        User u = userRepository.findById(id).get();
+        u.setFirstName(user.getFirstName());
+        u.setLastName(user.getLastName());
+        u.setEmail(user.getEmail());
+        u.setPassword(user.getPassword());
+
+        return userRepository.save(u);
+    }
+
+    @Override
+    public Void enableUser(Long id) {
+        return null;
+    }
+
+    @Override
+    public Void disableUser(Long id) {
+        return null;
+    }
 }
