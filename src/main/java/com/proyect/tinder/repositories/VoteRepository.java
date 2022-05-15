077aa07478410e966a1073f9c16d34a02d86e0fd
Josue Solis <josue1dario2solis3@gmail.com>
2022-05-15 12:30:22 -0300

    create repositories

--- src/main/java/com/proyect/tinder/repositories/VoteRepository.java
+++ src/main/java/com/proyect/tinder/repositories/VoteRepository.java
@@ -4,6 +4,12 @@ import com.proyect.tinder.models.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote,Long> {
+
+    List<Vote> findByPet1OrderByDateDesc(String id);
+
+    List<Vote> findByPet2OrderByDateDesc(String id);
 }
