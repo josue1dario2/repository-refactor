d8d08752d699fa9f34415e8e4cb0e00b5b3af4df
Josue Solis <josue1dario2solis3@gmail.com>
2022-06-14 21:30:18 -0300

    change the id of long to integer in repository

--- src/main/java/com/proyect/tinder/repositories/PetRepository.java
+++ src/main/java/com/proyect/tinder/repositories/PetRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PetRepository extends JpaRepository<Pet,Long> {
+public interface PetRepository extends JpaRepository<Pet,Integer> {
 }
