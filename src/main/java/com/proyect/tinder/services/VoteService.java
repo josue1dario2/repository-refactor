5d417bb265d09e1555ce8ff2ba420c0ad462f083
Josue Solis <josue1dario2solis3@gmail.com>
2022-07-02 00:38:09 -0300

    We create the service vote class
    We add a method to make a vote

--- src/main/java/com/proyect/tinder/services/VoteService.java
+++ src/main/java/com/proyect/tinder/services/VoteService.java
@@ -1,9 +1,9 @@
-package com.proyect.tinder.services.impl;
+package com.proyect.tinder.services;
 
 import com.proyect.tinder.exception.SpringException;
 
 public interface VoteService {
 
     void vote(Integer idUser,Integer idPet1, Integer idPet2) throws SpringException;
-    
+
 }
