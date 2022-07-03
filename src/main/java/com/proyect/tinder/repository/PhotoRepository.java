718f66f75f86e2099179d3d1afd1b5a733a45649
Josue Solis <josue1dario2solis3@gmail.com>
2022-07-03 18:08:19 -0300

    Create Photo repository

---
+++ src/main/java/com/proyect/tinder/repository/PhotoRepository.java
@@ -0,0 +1,10 @@
+package com.proyect.tinder.repository;
+
+
+import com.proyect.tinder.model.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhotoRepository extends JpaRepository<Photo,Integer> {
+}
