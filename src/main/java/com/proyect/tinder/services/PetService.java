0b6fa52c3d46d99634d90f3d2400c3793af7ed0e
Josue Solis <josue1dario2solis3@gmail.com>
2022-07-05 22:03:33 -0300

    Add method add, update, delete

--- src/main/java/com/proyect/tinder/services/PetService.java
+++ src/main/java/com/proyect/tinder/services/PetService.java
@@ -1,4 +1,13 @@
 package com.proyect.tinder.services;
 
+import com.proyect.tinder.exception.SpringException;
+import com.proyect.tinder.model.Pet;
+
 public interface PetService {
+
+    Pet addPet(Integer idUser, Pet pet)throws SpringException;
+
+    Pet updatePet(Integer idUser,Pet pet)throws SpringException;
+
+    void deletePet(Integer idUser,Integer idPet)throws SpringException;
 }
