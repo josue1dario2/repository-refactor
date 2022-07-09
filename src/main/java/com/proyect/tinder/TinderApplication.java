faf6f0aaa1eeecb1444d1495558db2d4f75779a1
Josue Solis <josue1dario2solis3@gmail.com>
2022-07-08 23:02:58 -0300

    Extends WebSecurityConfigurerAdapter

--- src/main/java/com/proyect/tinder/TinderApplication.java
+++ src/main/java/com/proyect/tinder/TinderApplication.java
@@ -1,13 +1,27 @@
 package com.proyect.tinder;
 
+import com.proyect.tinder.services.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class TinderApplication {
+public class TinderApplication extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserServiceImpl userService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TinderApplication.class, args);
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 
 }
